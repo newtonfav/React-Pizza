53dcfc4d08447e4cc20498879971fdf6c6a85d28
Favour Oghenekowho <gh.oghene@gmail.com>
2024-07-21 00:51:18 +0100

    implement render as you fetch using react router

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">React Pizza Co.</Link>
+
+      <div>Favour</div>
+    </header>
+  );
+}
+
+export default Header;
